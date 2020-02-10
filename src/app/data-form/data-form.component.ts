11dbc8ac3e998c73525ae1fab1aade19874dcf24
Caio Vinicius Rodrigues <rodrigues.caio1985@gmail.com>
2020-02-10 15:59:46 -0300

    Formulario Reativo

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+
+import { HttpClient } from '@angular/common/http';
+
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-data-form',
@@ -10,20 +14,121 @@ export class DataFormComponent implements OnInit {
 
   formulario: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private httpClient: HttpClient
+    ) { }
 
   ngOnInit() {
 
     this.formulario = this.formBuilder.group({
-      nome: [null],
-      email: [null]
+      nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+      email: [null, [Validators.required, Validators.email]],
+      endereco: this.formBuilder.group({
+          cep: [null, Validators.required],
+          numero: [null, Validators.required],
+          complemento: [null],
+          rua: [null, Validators.required],
+          bairro: [null, Validators.required],
+          cidade: [null, Validators.required],
+          estado: [null, Validators.required]
+      })
+
     });
 
-    /*
-    this.formulario = new FormGroup({
-      nome: new FormControl('Caio'),
-      email: new FormControl(null)
-    });*/
+
+  }
+
+  onSubmit() {
+    console.log(this.formulario.value);
+
+    if(this.formulario.valid) {
+      this.httpClient.post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
+      .pipe(map(res => res))
+      .subscribe(dados => {
+        console.log(dados);
+        this.formulario.reset();
+      });
+    } else {
+      console.log('Formulario Invalido');
+      this.verificaValidacoesFormulario(this.formulario);
+    }
+
+  }
+
+  verificaValidacoesFormulario(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach(campo => {
+      console.table(campo);
+      const controle = formGroup.get(campo);
+      controle.markAsDirty();
+      if(controle instanceof FormGroup) {
+        this.verificaValidacoesFormulario(controle);
+      }
+    });
+  }
+
+  cancelar() {
+    this.formulario.reset();
+  }
+
+  verificaValidTouched(campo) {
+    return !this.formulario.get(campo).valid &&
+       (this.formulario.get(campo).dirty || this.formulario.get(campo).touched);
+  }
+
+  aplicaCssError(campo) {
+    return {
+      "has-error": this.verificaValidTouched(campo),
+      "has-feedback": this.verificaValidTouched(campo)
+    };
+  }
+
+  consultaCEP() {
+    console.log('DEBUG', this.formulario.get('endereco.cep').value);
+    let cep = this.formulario.get('endereco.cep').value;
+    cep = cep.replace(/\D/g, '');
+
+    if (cep != '') {
+      var validacep = /^[0-9]{8}$/;
+
+      if (validacep.test(cep)) {
+
+        this.resetaDadosFormulario();
+
+        this.httpClient
+          .get(`https://viacep.com.br/ws/${cep}/json/`)
+          .pipe(map(dados => dados))
+          .subscribe(dados => this.populaDados(dados));
+      }
+    }
+  }
+
+  populaDados(dados) {
+    this.formulario.patchValue({
+      endereco: {
+        cep: dados.cep,
+        numero: '',
+        complemento: dados.complemento,
+        rua: dados.logradouro,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });
+  }
+
+  resetaDadosFormulario() {
+    this.formulario.patchValue({
+      endereco: {
+        cep: null,
+        numero: '',
+        complemento: null,
+        rua: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    });
   }
 
 }
--- src/app/data-form/data-form.module.ts
+++ src/app/data-form/data-form.module.ts
@@ -3,6 +3,7 @@ import { DataFormComponent } from './data-form.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
+import { HttpClient } from '@angular/common/http';
 
 
 
